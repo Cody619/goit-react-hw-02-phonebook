fe3a63ff3038bba4b388bc23869a6dbdfc33ee8d
Codyfs <codyfs619@gmail.com>
2021-10-20 21:56:35 +0300

    componets

---
+++ README.md
@@ -0,0 +1,9 @@
+```
+
+git add .
+git commit -m ""
+git push
+
+npm run deploy
+
+```
---
+++ src/App.js
@@ -0,0 +1,93 @@
+import { Component } from 'react'
+import './App.css'
+import { v4 as uuidv4 } from 'uuid'
+import { PhoneBook } from './componets/PhoneBook.js'
+import { Contacts } from './componets/Contacts'
+
+export default class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState((state) => {
+      if (
+        state.contacts.some((contact) => {
+          return contact.name === state.name
+        })
+      ) {
+        alert(`${state.name} is already in contacts`)
+        return state
+      } else {
+        return {
+          contacts: [
+            ...state.contacts,
+            { name: state.name, id: uuidv4(), number: state.number },
+          ],
+          name: '',
+          number: '',
+        }
+      }
+    })
+  }
+
+  handleDeleteItem = (id) => {
+    this.setState((state) => {
+      return {
+        contacts: state.contacts.filter((contact) => {
+          return contact.id !== id
+        }),
+      }
+    })
+  }
+
+  handleNameChange = (event) => {
+    this.setState({ name: event.currentTarget.value })
+  }
+  handleNumberChange = (event) => {
+    this.setState({ number: event.currentTarget.value })
+  }
+
+  handleFilterChange = (event) => {
+    this.setState({ filter: event.currentTarget.value })
+  }
+  filterContacts = () => {
+    if (this.state.filter === '') {
+      return this.state.contacts
+    } else {
+      return this.state.contacts.filter((contact) => {
+        return contact.name
+          .toLowerCase()
+          .includes(this.state.filter.toLowerCase())
+      })
+    }
+  }
+  render() {
+    return (
+      <div>
+        <PhoneBook
+          filter={this.state.filter}
+          name={this.state.name}
+          number={this.state.number}
+          handleSubmit={this.handleSubmit}
+          handleNameChange={this.handleNameChange}
+          handleNumberChange={this.handleNumberChange}
+          handleFilterChange={this.handleFilterChange}
+        />
+        <Contacts
+          contacts={this.filterContacts()}
+          handleDeleteItem={this.handleDeleteItem}
+        />
+      </div>
+    )
+  }
+}
